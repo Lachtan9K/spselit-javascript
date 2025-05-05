0778d3858326b91d1d8b9fd20758f106a26393f0
Erik Macháček <dice.sensei@gmail.com>
2025-05-05 21:30:33 +0200

    Add more detail regard from where function is called

--- 11_WebsiteIntegration_RunningFunctions/app.js
+++ 11_WebsiteIntegration_RunningFunctions/app.js
@@ -1,10 +1,12 @@
 console.log("Running from the app.js file");
 
-function doStuff() {
-  console.log("Running doStuff function");
+function doStuff(from = "direct call") {
+  console.log("Running doStuff function from: " + from);
   console.log("this:", this);
 }
 
 doStuff();
 
-setTimeout(doStuff, 5000);
+setTimeout(doStuff, 5000, "timeout");
+
+setInterval(doStuff, 10000, "interval");
